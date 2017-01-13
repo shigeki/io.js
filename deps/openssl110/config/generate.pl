d33a9342b168c052a80cf05c232a9ea01c877b7e
Shigeki Ohtsu <ohtsu@ohtsu.org>
2017-01-13 16:22:04 +0900

    refactor generate.pl/Makefiles

--- deps/openssl110/config/generate.pl
+++ deps/openssl110/config/generate.pl
@@ -1,9 +1,34 @@
 #! /usr/bin/env perl
-use lib "./archs/$ARGV[1]/$ARGV[0]/";
 use configdata;
 
-$conf_opt = $ARGV[0];
-$arch = $ARGV[1];
+my $asm = $ARGV[0];
+my $arch = $ARGV[1];
+
+my $is_win = ($arch =~/^VC-WIN/);
+my $makefile;
+
+if ($is_win) {
+    $makefile = "../config/Makefile_$arch";
+} else {
+    $makefile = 'Makefile';
+}
+
+my $base_dir = "../config/archs/$arch/$asm";
+
+my $cmd1 = "
+cd ../openssl; make -f $makefile build_generated crypto/buildinf.h;
+if [ ! -d $base_dir/crypto/include/internal ]; then mkdir -p $base_dir/crypto/include/internal ; fi
+if [ ! -d $base_dir/include/openssl ]; then mkdir -p $base_dir/include/openssl ; fi
+cp ../openssl/configdata.pm $base_dir/;
+cp ../openssl/include/openssl/opensslconf.h $base_dir/include/openssl/;
+mv ../openssl/crypto/include/internal/bn_conf.h $base_dir/crypto/include/internal/;
+mv ../openssl/crypto/include/internal/dso_conf.h $base_dir/crypto/include/internal/;
+cp ../openssl/crypto/buildinf.h $base_dir/crypto/;
+";
+
+print STDOUT "$cmd1\n";
+my $ret1 = system($cmd1);
+print STDOUT "$ret1\n";
 
 my @libssl_srcs = ();
 foreach my $obj (@{$unified_info{sources}->{libssl}}) {
@@ -16,6 +41,10 @@ foreach my $obj (@{$unified_info{sources}->{libcrypto}}) {
     my $src = ${$unified_info{sources}->{$obj}}[0];
     # .S files should be proprocessed into .s
     if ($unified_info{generate}->{$src}) {
+        # .S or .s files should be proprocessed into .asm for WIN
+        if ($is_win) {
+            $src =~ s\.[sS]$\.asm\;
+        }
         push(@generated_srcs, $src);
     } else {
         push(@libcrypto_srcs, $src);
@@ -28,11 +57,11 @@ foreach my $obj (@{$unified_info{sources}->{'apps/openssl'}}) {
 }
 
 foreach my $src (@generated_srcs) {
-    $cmd = "cd ../openssl; CC=gcc ASM=nasm make $src; cp --parents $src ../config/archs/$arch/$conf_opt; cd ../config";
+    $cmd = "cd ../openssl; CC=gcc ASM=nasm make -f $makefile $src; cp --parents $src ../config/archs/$arch/$asm; cd ../config";
     system("$cmd");
 }
 
-open(GYPI, "> ../config/archs/$arch/$conf_opt/openssl.gypi");
+open(GYPI, "> ../config/archs/$arch/$asm/openssl.gypi");
 
 print GYPI "{
   'variables': {
@@ -50,7 +79,7 @@ print GYPI "    ],\n";
 
 print GYPI "    'openssl_sources_$arch': [\n";
 foreach my $src (@generated_srcs) {
-    print GYPI "      './config/archs/$arch/$conf_opt/$src',\n";
+    print GYPI "      './config/archs/$arch/$asm/$src',\n";
 }
 print GYPI "    ],\n";
 
@@ -73,14 +102,14 @@ print GYPI "    './crypto',\n";
 print GYPI "    './crypto/include/internal',\n";
 print GYPI "  ],\n";
 print GYPI "  'defines': ['<@(openssl_defines_$arch)'],\n";
-print GYPI "  'cflags' : ['<@(openssl_cflags_$arch)'],\n";
-print GYPI "  'libraries': ['<@(openssl_ex_libs_$arch)'],\n";
+print GYPI "  'cflags' : ['<@(openssl_cflags_$arch)'],\n" unless (is_win);
+print GYPI "  'libraries': ['<@(openssl_ex_libs_$arch)'],\n" unless (is_win);
 print GYPI "  'sources': ['<@(openssl_sources)', '<@(openssl_sources_$arch)'],\n";
 print GYPI "}\n";
 
 close(GYPI);
 
-open(CLGYPI, "> ../config/archs/$arch/$conf_opt/openssl-cl.gypi");
+open(CLGYPI, "> ../config/archs/$arch/$asm/openssl-cl.gypi");
 print CLGYPI "{
   'variables': {\n";
 print CLGYPI "    'openssl_defines_$arch': [\n";
@@ -104,9 +133,14 @@ foreach my $src (@apps_openssl_srcs) {
 print CLGYPI "    ],\n";
 print CLGYPI "  },\n";
 print CLGYPI "  'defines': ['<@(openssl_defines_$arch)'],\n";
-print CLGYPI "  'cflags' : ['<@(openssl_cflags_$arch)'],\n";
-print CLGYPI "  'libraries': ['<@(openssl_ex_libs_$arch)'],\n";
+print CLGYPI "  'cflags' : ['<@(openssl_cflags_$arch)'],\n" unless (is_win);
+print CLGYPI "  'libraries': ['<@(openssl_ex_libs_$arch)'],\n" unless (is_win);
 print CLGYPI "  'sources': ['<@(openssl_cli_srcs_$arch)'],\n";
 print CLGYPI "}\n";
 
 close(CLGYPI);
+
+my $cmd2 ="cd ../openssl; make -f $makefile clean; make -f $makefile distclean; find ../openssl/crypto \\( -name \*.S -o -name \*.s -o -name \*.asm \\) -exec rm '{}' \\;";
+print STDOUT "$cmd2\n";
+my $ret2 = system($cmd2);
+print STDOUT "$ret2\n";
