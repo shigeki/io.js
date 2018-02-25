a9224199ca31e6fa8025825b635c9536d47b3b4b
Shigeki Ohtsu <ohtsu@ohtsu.org>
2018-02-25 12:06:07 +0900

    test: fix testings to work in openssl-1.1.1-pre1

--- test/parallel/test-tls-set-ciphers.js
+++ test/parallel/test-tls-set-ciphers.js
@@ -52,7 +52,7 @@ const server = tls.createServer(options, common.mustCall(function(conn) {
 
 server.listen(0, '127.0.0.1', function() {
   const cmd = `"${common.opensslCli}" s_client -cipher ${
-    options.ciphers} -connect 127.0.0.1:${this.address().port}`;
+    options.ciphers} -connect 127.0.0.1:${this.address().port} -tls1_2`;
 
   exec(cmd, function(err, stdout, stderr) {
     assert.ifError(err);
